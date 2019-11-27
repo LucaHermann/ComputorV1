9a8995e5620c788133da3ff6ebb4370f6ba895ee
lhermann <lhermann@student.42.fr>
2019-11-27 11:44:36 +0100

    work on the parsing

--- srcs/computor.py
+++ srcs/computor.py
@@ -2,6 +2,11 @@ import ply.lex as lex  # lexer
 import ply.yacc as yacc  # parser
 import sys
 
+# calculs
+degrees = [0, 0, 0]
+
+# def c_store(p):
+
 # List of token names.
 tokens = (
     'INT',
@@ -12,7 +17,6 @@ tokens = (
     'DIVIDE',
     'UNKW',
     'EQUALS',
-    'SQRTROOT',
     'LPAREN',
     'RPAREN',
 )
@@ -22,15 +26,20 @@ t_PLUS = r'\+'
 t_MINUS = r'-'
 t_MULTIPLY = r'\*'
 t_DIVIDE = r'/'
-t_UNKW = r'X'
 t_EQUALS = r'='
-t_SQRTROOT = r'\^'
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
 
 t_ignore = r' '
 
 # def method for lexer
+
+
+def t_UNKW(t):
+    r'X(\^?[0-9]?)'
+    if t.value == "X":
+        t.value = "X^1"
+    return t
 
 
 def t_INT(t):
@@ -54,14 +63,25 @@ lexer = lex.lex()
 
 # def method for the parser
 
+precedence = (
+    ('left', 'PLUS', 'MINUS'),
+    ('left', 'MULTIPLY', 'DIVIDE')
+)
 
-def p_calc(p):
-    '''
-    calc : expression
-        | empty
-    '''
-    print(p[1])
 
+def p_findFactor(p):
+    '''
+    expression : expression MULTIPLY UNKW
+    '''
+    degrees[int(p[3].split('^')[1])] += p[1]
+    print(p[1], p[3])
+    p[0] = 0
+
+
+def p_addplus(p):
+  '''
+  expression : expression PLUS expression MULTIPLY UNKW
+  '''
 
 def p_expression(p):
     '''
@@ -69,8 +89,11 @@ def p_expression(p):
               | expression MINUS expression
               | expression PLUS expression
               | expression DIVIDE expression
+              | expression UNKW expression
+              | expression EQUALS expression
     '''
-    p[0] = (p[2], p[1], p[3])
+    p[0] = (p[1], p[2], p[3])
+    print(p[1], p[2])
 
 
 def p_expression_int_float(p):
@@ -81,15 +104,8 @@ def p_expression_int_float(p):
     p[0] = p[1]
 
 
-def p_empty(p):
-    '''
-    empty :
-    '''
-    p[0] = None
-
-
 def p_error(p):
-    print("something went wrong illegal character[parser]")
+    print("something went wrong illegal character[{}]".format(p))
     p.parser.skip(1)
 
 
@@ -103,3 +119,4 @@ while True:
     except EOFError:
         break
     parser.parse(s)
+    degrees = [0, 0, 0]
