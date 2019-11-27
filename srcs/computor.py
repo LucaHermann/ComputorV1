20b0c0d2e877483cd0700d5cc73d80cea01503ed
lhermann <lhermann@student.42.fr>
2019-11-27 14:19:04 +0100

    work on a better parser lexer

--- srcs/computor.py
+++ srcs/computor.py
@@ -2,37 +2,33 @@ import ply.lex as lex  # lexer
 import ply.yacc as yacc  # parser
 import sys
 
-# calculs
+# suivant la puissance de l'inconnu (1er, 2eme ou pas de puissance(0))
+# l'inconnu est rangée dans degrees a lindex correspondant
 degrees = [0, 0, 0]
 
 # def c_store(p):
 
-# List of token names.
+# Liste des noms de tokens.
 tokens = (
-    'INT',
-    'FLOAT',
+    'NUMBER',
     'PLUS',
     'MINUS',
     'MULTIPLY',
     'DIVIDE',
     'UNKW',
-    'EQUALS',
     'LPAREN',
     'RPAREN',
 )
 
-# Regular expression rules for simple tokens
+# definitions des regles regexpr pour les tokens
 t_PLUS = r'\+'
 t_MINUS = r'-'
 t_MULTIPLY = r'\*'
 t_DIVIDE = r'/'
-t_EQUALS = r'='
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
 
 t_ignore = r' '
-
-# def method for lexer
 
 
 def t_UNKW(t):
@@ -40,29 +36,28 @@ def t_UNKW(t):
     if t.value == "X":
         t.value = "X^1"
     return t
+# si dans l'expression je trouve une inconnue
 
 
-def t_INT(t):
-    r'\d+'
-    t.value = int(t.value)
-    return t
-
-
-def t_FLOAT(t):
-    r'\d\.\d+'
-    t.value = float(t.value)
+def t_NUMBER(t):
+    r'-?\d*\.?\d+'
+    if ("." in t.value):
+        t.value = float(t.value)
+    else:
+        t.value = int(t.value)
     return t
 
 
 def t_error(t):
-    print("something went wrong illegal character[lexer]")
+    print("something went wrong in lexer")
     t.lexer.skip(1)
+# si dans l'expression je trouve une erreur
 
 
 lexer = lex.lex()
+# creation du lexer
 
-# def method for the parser
-
+# definition de regles de calculs primaires
 precedence = (
     ('left', 'PLUS', 'MINUS'),
     ('left', 'MULTIPLY', 'DIVIDE')
@@ -71,46 +66,56 @@ precedence = (
 
 def p_findFactor(p):
     '''
-    expression : expression MULTIPLY UNKW
+    expression : NUMBER MULTIPLY UNKW
     '''
     degrees[int(p[3].split('^')[1])] += p[1]
-    print(p[1], p[3])
     p[0] = 0
+    print('--degrees in findFactor-- :', degrees)
+# si mon expression est multiplier par une inconnue je recupere linconnu et la classe dans degrees
 
 
-def p_addplus(p):
-  '''
-  expression : expression PLUS expression MULTIPLY UNKW
-  '''
+def p_addPlus(p):
+    '''
+    expression : expression PLUS NUMBER MULTIPLY UNKW
+    '''
+    degrees[int(p[5].split('^')[1])] += p[3]
+    print('--degrees in addPlus-- :', degrees)
+
+
+def p_addMinus(p):
+    '''
+    expression : expression MINUS NUMBER MULTIPLY UNKW
+    '''
+    degrees[int(p[5].split('^')[1])] += p[3]
+    print('--degrees in addMinus-- :', degrees)
+
 
 def p_expression(p):
     '''
-    expression : expression MULTIPLY expression
-              | expression MINUS expression
-              | expression PLUS expression
-              | expression DIVIDE expression
-              | expression UNKW expression
-              | expression EQUALS expression
+    expression : NUMBER MULTIPLY NUMBER
+              | NUMBER MINUS NUMBER
+              | NUMBER PLUS NUMBER
+              | NUMBER DIVIDE NUMBER
+              | NUMBER UNKW NUMBER
     '''
     p[0] = (p[1], p[2], p[3])
-    print(p[1], p[2])
+    print('p_expression', p[1], p[2])
 
 
-def p_expression_int_float(p):
+def p_expression_number(p):
     '''
-    expression : INT
-              | FLOAT
+    expression : NUMBER
     '''
     p[0] = p[1]
 
 
 def p_error(p):
-    print("something went wrong illegal character[{}]".format(p))
+    print("something went wrong in parser [{}]".format(p))
     p.parser.skip(1)
 
 
 parser = yacc.yacc()
-# take user input
+# equivalence d'un main
 
 
 while True:
@@ -118,5 +123,10 @@ while True:
         s = input('')
     except EOFError:
         break
-    parser.parse(s)
-    degrees = [0, 0, 0]
+    if ('=' in s):
+        first, second = s.split('=')
+        parser.parse(first)
+        parser.parse(second)
+        degrees = [0, 0, 0]
+    else:
+        print("input got the wrong format please input other calculs")
