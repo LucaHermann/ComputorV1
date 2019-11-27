1ec1860e2b1f15d0811e30ac907fc47866c731b0
lhermann <lhermann@student.42.fr>
2019-11-27 08:30:54 +0100

    Starting the project.

---
+++ srcs/computor.py
@@ -0,0 +1,22 @@
+import ply.lex as lex  # lexer
+import ply.yacc as yacc  # parser
+import sys
+
+# List of token names.
+tokens = (
+    'INT',
+    'PLUS',
+    'MINUS',
+    'TIMES',
+    'DIVIDE',
+    'LPAREN',
+    'RPAREN',
+)
+
+# Regular expression rules for simple tokens
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
